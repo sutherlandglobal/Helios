df36f64fa12cc60fd47c242ca7910a0819ff57e0
jasondiam0nd <jason.diamond@sutherlandglobal.com>
2014-05-10 13:26:22 -0400

    Supported frontend groups for supported report types.

---
+++ src/helios/api/report/frontend/ReportFrontEndGroups.java
@@ -0,0 +1,29 @@
+package helios.api.report.frontend;
+
+import java.util.LinkedHashMap;
+
+public interface ReportFrontEndGroups 
+{
+	public final static LinkedHashMap<String, String> STACK_RANK_FRONTENDS = new LinkedHashMap<String, String>()
+	{
+		private static final long serialVersionUID = 8981150749471588078L;
+		{
+			put(ReportFrontEnds.CSV_NAME,"" +ReportFrontEnds.CSV);
+			put(ReportFrontEnds.HTML_NAME,"" +ReportFrontEnds.HTML);
+			put(ReportFrontEnds.XML_NAME,"" +ReportFrontEnds.XML);
+			put(ReportFrontEnds.JSON_NAME,"" +ReportFrontEnds.JSON);
+		}
+	};
+	
+	public final static LinkedHashMap<String, String> BASIC_METRIC_FRONTENDS = new LinkedHashMap<String, String>()
+	{
+		private static final long serialVersionUID = -2093351638483326816L;
+		{
+			put(ReportFrontEnds.CSV_NAME,"" +ReportFrontEnds.CSV);
+			put(ReportFrontEnds.HTML_NAME,"" +ReportFrontEnds.HTML);
+			put(ReportFrontEnds.XML_NAME,"" +ReportFrontEnds.XML);
+			put(ReportFrontEnds.JSON_NAME,"" +ReportFrontEnds.JSON);
+			put(ReportFrontEnds.JFCHART_NAME,"" +ReportFrontEnds.JFREECHART);
+		}
+	};
+}
