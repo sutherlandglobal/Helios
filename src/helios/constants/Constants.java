6c60aecad6263293df991fa8ea0159c5c1984c4f
jasondiam0nd <jason.diamond@sutherlandglobal.com>
2014-05-10 13:34:53 -0400

    Framework constants and hardcodable config.

---
+++ src/helios/constants/Constants.java
@@ -0,0 +1,18 @@
+package helios.constants;
+
+/**
+ * Managing and accessing constants global to the Helios Framework.
+ * 
+ * @author Jason Diamond
+ *
+ */
+public final class Constants 
+{
+	public final static String JUNIT_LOGGING_HANDLE = "junit";
+	
+	public final static String PLATFORM_DIR = "/opt/tomcat/apache-tomee-plus";
+	
+	public final static String API_LOGGER_HANDLE = "helios";
+	
+	public final static String DATABASE_LOGGER_HANDLE = "database";
+}
