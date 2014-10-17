047511dd5670a2ab5321a2f6d8382e683d4eb9b9
jas0ndiam0nd <jason.diamond@sutherlandglobal.com>
2014-10-17 14:29:43 -0400

    Synchronized method to avoid duplicate ids.

--- src/com/sutherland/helios/logging/LogIDFactory.java
+++ src/com/sutherland/helios/logging/LogIDFactory.java
@@ -12,7 +12,7 @@ public class LogIDFactory
 {
 	private LogIDFactory(){}
 	
-	public static String getLogID()
+	public synchronized static String getLogID()
 	{
 		//avoid the oddly-changing value
 		return new Long(System.currentTimeMillis() / 1000L).toString();
