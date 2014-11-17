627815cb384ad84ee4d1ba756257ce3a8c870658
jas0ndiam0nd <jason.diamond@sutherlandglobal.com>
2014-11-16 20:56:47 -0500

    Add primeui js.
    Remove daterequest.js. The ui never needs to know the date for the time
    being. Just pass around the "NOW" keyword.

--- resources/js/webRequest.js
+++
@@ -1,9 +0,0 @@
-function httpGet(url)
-{
-    var xmlHttp = null;
-
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
