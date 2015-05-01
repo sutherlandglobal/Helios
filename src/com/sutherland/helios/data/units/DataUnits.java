1352064056bb5adeae4e696df95999431ffa7add
jas0ndiam0nd <jason.diamond@sutherlandglobal.com>
2015-04-30 23:32:57 -0400

    Standardize data units for metrics.

---
+++ src/com/sutherland/helios/data/units/DataUnits.java
@@ -0,0 +1,39 @@
+package com.sutherland.helios.data.units;
+
+public abstract class DataUnits 
+{
+	public final static String DATE_GRAIN = "Date Grain";
+	public final static String USER_GRAIN = "User Grain";
+	
+	public final static String DOLLARS = "Dollars ($)";
+	public final static String SALES_COUNT = "Sales Count";
+	public final static String REFUND_COUNT = "Refund Count";
+	
+	public final static String MINUTES = "Minutes";
+	
+	public final static String CALL_COUNT = "Call Count";
+	public final static String CASE_COUNT = "Case Count";
+	
+	public final static String CSAT_RATE = "CSAT Rate (%)";
+
+	public final static String CALLS_PER_CASE = "Calls per Case";
+	
+	public final static String CONVERSION = "Conversion (%)";
+	
+	public final static String LATE_DAYS = "Day Count";
+	
+	public final static String SURVEY_COUNT = "Survey Count";
+	
+	public final static String CONSUMPTION_RATE = "Consumption Rate (%)";
+	
+	public final static String PIN_COUNT = "PIN Count";
+	
+	public final static String DRIVER_COUNT = "Count";
+	
+	public final static String ISSUE_COUNT = "Issue Count";
+	
+	
+	public final static String PERCENTAGE = "Percentage (%)";
+	
+	public final static String NET_PROMOTER_SCORE = "Net Promoter Score";
+}
